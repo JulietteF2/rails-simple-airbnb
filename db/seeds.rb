bec73e0048e2bdbbd5e5c993f9d75f574431ae86
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-02-19 17:04:10 +1100

    index, add, edit = set, show page lacks options, no filtering, no picture_url yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+house = ["'s Mansion", "'s Suite", "'s Studio", "'s Bedroom", "'s Castle", "'s Palace", "'s Boat", "'s Room", "'s Skyscrapper", "'s Family House"]
+
+25.times do
+  Flat.create(
+    name: Faker::TvShows::BojackHorseman.character + house.sample,
+    address: Faker::Address.full_address,
+    description: Faker::TvShows::BojackHorseman.quote,
+    price_per_night: (47...187).to_a.sample,
+    number_of_guests: (1...6).to_a.sample
+  )
+end
